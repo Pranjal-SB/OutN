ca65fe0ac6f4b6a3c1b0a81465448ae4130c47c6
PranjalSB <82652989+Pranjal-SB@users.noreply.github.com>
2024-06-21 16:56:11 +0530

    Update main.py - v3.1 -> multiple updates
    checkout changelog

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 from io import BytesIO
 
+#OutN version
+version = 'v3.1'
+
 #user inputs
 config = configparser.ConfigParser()
 config_file = 'config.ini'
@@ -40,28 +43,70 @@ rping = int(config['DEFAULT']['RPING'])
 intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
-bot = commands.Bot(command_prefix='<', intents=intents)
-
-loaded_model = load_model('model.h5', compile=False)
+bot = commands.Bot(command_prefix='~', intents=intents)
 
 #data load
+loaded_model = load_model('model.h5', compile=False)
 
 with open('classes.json', 'r') as f:
   classes = json.load(f)
 
-with open('data/trares', 'r', encoding='utf8') as file:
+with open('data/trares.txt', 'r') as file:
   rares = file.read()
 
-with open('data/legendary', 'r') as file:
+with open('data/legendary.txt', 'r') as file:
   legendary_list = file.read()
 
-with open('data/mythical', 'r') as file:
+with open('data/mythical.txt', 'r') as file:
   mythical_list = file.read()
 
-with open('data/ultrabeast', 'r') as file:
+with open('data/ultrabeast.txt', 'r') as file:
   ub_list = file.read()
 
-#bot start
+
+#custom modules
+async def mythic_embed(message, name):
+  embed = discord.Embed(color=discord.Color.gold())
+  embed.set_footer(text="❤️ The OutN Project")
+  embed.add_field(
+      name='New Spawn!',
+      value=
+      f'**__{name}__**, A mythic Pokemon spawned! catch it using:')
+  embed.add_field(name='Command', value=f"@Pokétwo#8236 c {name}")
+  await message.channel.send(embed=embed)
+
+async def legendary_embed(message, name):
+  embed = discord.Embed(color=discord.Color.gold())
+  embed.set_footer(text="❤️ The OutN Project")
+  embed.add_field(
+      name='New Spawn!',
+      value=
+      f'**__{name}__**, A legendary Pokemon spawned! catch it using:'
+  )
+  embed.add_field(name='Command', value=f"@Pokétwo#8236 c {name}")
+  await message.channel.send(embed=embed)
+
+async def ub_embed(message, name):
+  embed = discord.Embed(color=discord.Color.gold())
+  embed.set_footer(text="❤️ The OutN Project")
+  embed.add_field(
+      name='New Spawn!',
+      value=
+      f'**__{name}__**, An Ultra Beast spawned! catch it using:'
+  )
+  embed.add_field(name='Command', value=f"@Pokétwo#8236 c {name}")
+  await message.channel.send(embed=embed)
+
+async def common_embed(message, name):
+  embed = discord.Embed(color=discord.Color.blue())
+  embed.set_footer(text="❤️ The OutN Project")
+  embed.add_field(
+      name='New Spawn!',
+      value=f'**__{name}__** spawned! catch it using:')
+  embed.add_field(name='Command', value=f"@Pokétwo#8236 c {name}")
+  await message.channel.send(embed=embed)
+
+
 @bot.event
 async def on_ready():
   bot.db = await aiosqlite.connect("pokemon.db")
@@ -69,10 +114,12 @@ async def on_ready():
   await bot.db.commit()
 
   print('Logged in as', bot.user)
-  game = discord.Game("Pokémon in Beta")
+  print("The OutN Project",version)
+
+  game = discord.Game("Pokémon")
   await bot.change_presence(status=discord.Status.online, activity=game)
 
-#on message and image recog.
+
 @bot.event
 async def on_message(message):
   if message.author.id == 716390085896962058:
@@ -91,22 +138,20 @@ async def on_message(message):
         classes_x = np.argmax(predictions, axis=1)
         name = list(classes.keys())[classes_x[0]]
         async with message.channel.typing():
-          if name in rares and name != 'natu':
-            embed = discord.Embed(color=None)
-            embed.set_footer(text="❤️ The OutN Project")
-            embed.add_field(name=None,
-                            value='A rare Pokemon spawned! catch it using:')
-            embed.add_field(name=None, value=f"@Pokétwo#8236 c {name}")
-            await message.channel.send(embed=embed)
+          if name in mythical_list:
+            await mythic_embed(message, name)
+            await message.channel.send(f"<@&{rping}>")
+
+          elif name in legendary_list and name != 'natu':
+            await legendary_embed(message, name)
+            await message.channel.send(f"<@&{rping}>")
+
+          elif name in ub_list:
+            await ub_embed(message, name)
             await message.channel.send(f"<@&{rping}>")
 
           else:
-            embed = discord.Embed(color=None)
-            embed.set_footer(text="❤️ The OutN Project")
-            embed.add_field(name=None,
-                            value='A Pokemon spawned! catch it using:')
-            embed.add_field(name=None, value=f"@Pokétwo#8236 c {name}")
-            await message.channel.send(embed=embed)
+            await common_embed(message, name)
 
 
 async def preprocess_image(image):
@@ -115,5 +160,6 @@ async def preprocess_image(image):
   image = image / 255.0
   image = np.expand_dims(image, axis=0)
   return image
+
 
 bot.run(TKN)
