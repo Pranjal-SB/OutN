a8a15cc29dfb93858be14d1fa5e0446d4c9a7263
PranjalSB <psbhatnagar.in@gmail.com>
2024-06-28 22:37:52 +0530

    upgraded starboard

--- lib/star_helper.py
+++ lib/star_helper.py
@@ -4,45 +4,62 @@ from config import starch
 
 async def starit_mythic(bot, message, name):
   starboard = bot.get_channel(starch)
+  og_embed = message.embeds[0]
   staryu = Embed(color=Color.gold())
+  staryu.set_author(name="Pokétwo", icon_url='https://cdn.discordapp.com/avatars/716390085896962058/3031fa9e2fabde1652a57ab33f4d7f37.webp?size=160')
+  staryu.add_field(name=og_embed.title, value=og_embed.description, inline=False)
+  staryu.add_field(name='',
+                   value=f'[Jump to Message]({message.jump_url})', inline=False)
+  staryu.add_field(name='It is a Mythic Pokémon!',
+                   value=f'It is **__{name}__** !', inline=False)
+  staryu.set_image(url=og_embed.image.url)
   staryu.set_footer(text="❤️ The OutN Project")
-  staryu.add_field(name='A Mythic Pokémon spawned!',
-                   value=f'It\'s **__{name}__** !')
-  staryu.add_field(name='Message Link',
-                   value=f'[Jump to Message]({message.jump_url})')
+
   await starboard.send(embed=staryu)
   await message.channel.send(f"Spawn sent to <#{starch}>!")
 
 
 async def starit_legen(bot, message, name):
   starboard = bot.get_channel(starch)
+  og_embed = message.embeds[0]
   staryu = Embed(color=Color.gold())
+  staryu.set_author(name="Pokétwo", icon_url='https://cdn.discordapp.com/avatars/716390085896962058/3031fa9e2fabde1652a57ab33f4d7f37.webp?size=160')
+  staryu.add_field(name=og_embed.title, value=og_embed.description, inline=False)
+  staryu.add_field(name='',
+                   value=f'[Jump to Message]({message.jump_url})', inline=False)
+  staryu.add_field(name='It is a Legendary Pokémon!',
+                   value=f'It is **__{name}__** !', inline=False)
+  staryu.set_image(url=og_embed.image.url)
   staryu.set_footer(text="❤️ The OutN Project")
-  staryu.add_field(name='A Legendary Pokémon spawned!',
-                   value=f'It\'s **__{name}__** !')
-  staryu.add_field(name='Message Link',
-                   value=f'[Jump to Message]({message.jump_url})')
   await starboard.send(embed=staryu)
   await message.channel.send(f"Spawn sent to <#{starch}>!")
 
 async def starit_ub(bot, message, name):
   starboard = bot.get_channel(starch)
+  og_embed = message.embeds[0]
   staryu = Embed(color=Color.gold())
+  staryu.set_author(name="Pokétwo", icon_url='https://cdn.discordapp.com/avatars/716390085896962058/3031fa9e2fabde1652a57ab33f4d7f37.webp?size=160')
+  staryu.add_field(name=og_embed.title, value=og_embed.description, inline=False)
+  staryu.add_field(name='',
+                   value=f'[Jump to Message]({message.jump_url})', inline=False)
+  staryu.add_field(name='It is an Ultra Beast Pokémon!',
+                   value=f'It is **__{name}__** !', inline=False)
+  staryu.set_image(url=og_embed.image.url)
   staryu.set_footer(text="❤️ The OutN Project")
-  staryu.add_field(name='An Ultra Beast Pokémon spawned!',
-                   value=f'It\'s **__{name}__** !')
-  staryu.add_field(name='Message Link',
-                   value=f'[Jump to Message]({message.jump_url})')
   await starboard.send(embed=staryu)
   await message.channel.send(f"Spawn sent to <#{starch}>!")
 
 async def starit_reg(bot, message, name):
   starboard = bot.get_channel(starch)
-  staryu = Embed(color=Color.gold())
+  og_embed = message.embeds[0]
+  staryu = Embed(color=Color.silver())
+  staryu.set_author(name="Pokétwo", icon_url='https://cdn.discordapp.com/avatars/716390085896962058/3031fa9e2fabde1652a57ab33f4d7f37.webp?size=160')
+  staryu.add_field(name=og_embed.title, value=og_embed.description, inline=False)
+  staryu.add_field(name='',
+                   value=f'[Jump to Message]({message.jump_url})', inline=False)
+  staryu.add_field(name='It is a Regional Pokémon!',
+                   value=f'It is **__{name}__** !', inline=False)
+  staryu.set_image(url=og_embed.image.url)
   staryu.set_footer(text="❤️ The OutN Project")
-  staryu.add_field(name='A Regional Pokémon spawned!',
-                   value=f'It\'s **__{name}__** !')
-  staryu.add_field(name='Message Link',
-                   value=f'[Jump to Message]({message.jump_url})')
   await starboard.send(embed=staryu)
   await message.channel.send(f"Spawn sent to <#{starch}>!")
